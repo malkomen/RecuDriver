db352853e54bf6ad40fe64c4fc1f4cc722360c2f
malkomen <luniewski.s@gmail.com>
2022-02-06 22:19:58 +0100

    x

--- Src/main.c
+++ Src/main.c
@@ -405,7 +405,7 @@ static void MX_TIM2_Init(void)
 
   /* USER CODE END TIM2_Init 1 */
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 41;
+  htim2.Init.Prescaler = 83;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim2.Init.Period = 999;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
--- Src/sensorsTask.c
+++ Src/sensorsTask.c
@@ -86,7 +86,6 @@ void BME280_Handler()
 		}
 	}
 
-
 }
 
 
