bd42ebba9eacbdc7593c7b8a218c06e43b09f6a9
Slawek <luniewski.s@gmail.com>
2022-02-11 14:14:37 +0100

    harmonogram

--- Inc/nextionDisplay.h
+++ Inc/nextionDisplay.h
@@ -16,7 +16,49 @@
 #define NEXTION_RX_BUF_SIZE 	32
 #define TIMEOUT_UART_CHAR  		10 //ms
 
+#define PLANING_MODE_QTY		4 //liczba trow planowania
+
 typedef enum {odczytCzujnikow = 1, costam = 2}nextionCyclicalTrace_t;
+
+typedef enum
+{
+	modeWork_zDala 			= 1,
+	modeWork_normalny 		= 2,
+	modeWork_intensywny 	= 3,
+	modeWork_Turbo 			= 4,
+	modeWork_okap 			= 5,
+	modeWork_Kominek 		= 6,
+	modeWork_Priorytetowy 	= 7,
+	modeWork_Urlop 			= 8
+}modeWork_t;
+
+typedef enum
+{
+	planing_wDomu 			= 1,
+	planing_wBiurze			= 2,
+	planing_tydzienWPracy	= 3,
+	planing_uzutkownia		= 4
+}planing_t;
+
+
+
+typedef struct
+{
+	modeWork_t pon[24];
+	modeWork_t wt[24];
+	modeWork_t sr[24];
+	modeWork_t czw[24];
+	modeWork_t pt[24];
+	modeWork_t sob[24];
+	modeWork_t niedz[24];
+}timeTable_t;
+
+/*typedef struct
+{
+	timeTable_t plan[4];
+}timeTablePlaninig_t;*/
+
+
 
 
 void StartNextionDisplayTask(void const * argument);
